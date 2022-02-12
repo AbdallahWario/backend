066d45cb88597a6ce54abca5bdfe4ebee3917864
unknown <abdallwario96@gmail.com>
2022-02-12 19:12:31 +0300

    whitelisting

--- backend/settings.py
+++ backend/settings.py
@@ -65,9 +65,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    "http://localhost:3000",
-)
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000","https://isiolotimesfrontend.herokuapp.com","http://isiolotimesfrontend.herokuapp.com",
+]
 
 
 CSRF_TRUSTED_ORIGINS = [
@@ -178,3 +178,8 @@ REST_FRAMEWORK = {
 'rest_framework.authentication.SessionAuthentication',
 ),
 }
+
+import dj_database_url
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
