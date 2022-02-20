8e5c6f09fe8cb0bd4cde0cc89a2854fc9cd8ddcc
unknown <abdallwario96@gmail.com>
2022-02-20 15:01:12 +0300

    author field

--- backend/settings.py
+++ backend/settings.py
@@ -154,8 +154,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 #STATICFILES_DIRS = [    os.path.join(BASE_DIR, 'static')]
-MEDIA_ROOT = os.path.join(BASE_DIR, '/static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join('/static/images')
+STATIC_ROOT = os.path.join('static')
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 SUMMERNOTE_THEME = 'bs4'    # Use Bootstrap4 theme
